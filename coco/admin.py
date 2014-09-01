58c7f91af9fa227b9b7f2f570e91907dfbb4b5b0
Olivier Aubert <contact@olivieraubert.net>
2014-09-01 16:11:21 +0200

    Register Admin views

--- coco/admin.py
+++ coco/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
+from .models import Course, Video, Module, License, Annotation, Comment
+
+admin.site.register(Course)
+admin.site.register(Video)
+admin.site.register(License)
+admin.site.register(Annotation)
+admin.site.register(Comment)
 
 # Register your models here.
 
