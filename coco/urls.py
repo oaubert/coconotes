9fb2b09d74e0270c771827361d5ef3f471531945
Olivier Aubert <contact@olivieraubert.net>
2015-04-29 15:58:40 +0200

    Pass elements as view parameters

--- coco/urls.py
+++ coco/urls.py
@@ -4,7 +4,7 @@ import coco.views as views
 from .models import Course
 
 urlpatterns = patterns('',
-                       url(r'^$', TemplateView.as_view(template_name='root.html'), name='root'),
+                       url(r'^$', views.home, name='root'),
                        url(r'^course/$', ListView.as_view(model=Course), name='course-list'),
                        url(r'^course/(?P<pk>[\w\d_]+)/$', DetailView.as_view(model=Course, context_object_name='course'), name='course-detail'),
                        # REST API
--- coco/views.py
+++ coco/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from rest_framework import generics
-from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from .models import Course, Video
+from .models import Course, Video, Newsitem, Module
 from .serializers import CourseSerializer, VideoSerializer
 
 class CourseList(generics.ListCreateAPIView):
@@ -21,3 +21,11 @@ class VideoList(generics.ListCreateAPIView):
 class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
     model = Video
     serializer_class = VideoSerializer
+
+def home(request, **kw):
+    return render_to_response('root.html', {
+        'news': Newsitem.objects.order_by('-published')[:3],
+        'une_items': Module.objects.order_by('-created')[:4],
+        'last_videos': Video.objects.order_by('-created')[:4],
+        'username': request.user.username,
+    }, context_instance=RequestContext(request))
