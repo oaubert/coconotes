6aeebe024c2b23e6c1eae23ff7559ab6c66fa3c2
Olivier Aubert <contact@olivieraubert.net>
2015-04-29 15:03:17 +0200

    Remove unused module

--- coco/views.py
+++ coco/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.renderers import UnicodeJSONRenderer
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
