016af602e36a26dfc2798e64fef91b129a5c52f8
Olivier Aubert <contact@olivieraubert.net>
2015-04-30 10:42:40 +0200

    Define admin forms

--- coco/admin.py
+++ coco/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Course, Video, Module, License, Annotation, Comment, Resource, Newsitem
+from .models import Course, Video, Module, License, Annotation, Comment, Resource, Newsitem, Activity
 
-admin.site.register(Course)
-admin.site.register(Video)
 admin.site.register(License)
 admin.site.register(Annotation)
 admin.site.register(Resource)
@@ -14,5 +12,45 @@ admin.site.register(Newsitem)
 class ElementAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
     search_fields = ('title', )
+
+    prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'shorttitle', 'slug', 'creator', 'created', 'activity', 'thumbnail')
+    list_editable = ('title', 'shorttitle', 'slug', 'creator', 'created', 'activity', 'thumbnail')
+    list_display_links = ('pk', )
+    list_filter = ( 'creator', 'activity' )
+    search_fields = ('title', 'shorttitle')
+
+    prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'shorttitle', 'slug', 'creator', 'created', 'thumbnail', 'category')
+    list_editable = ('title', 'shorttitle', 'slug', 'creator', 'created', 'thumbnail', 'category')
+    list_display_links = ('pk', )
+    list_filter = ( 'creator', 'category' )
+    search_fields = ('title', 'shorttitle')
+
+    prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'shorttitle', 'slug', 'creator', 'created', 'thumbnail', 'course')
+    list_editable = ('title', 'shorttitle', 'slug', 'creator', 'created', 'thumbnail', 'course')
+    list_display_links = ('pk', )
+    list_filter = ( 'creator', 'course' )
+    search_fields = ('title', 'shorttitle')
+
+    prepopulated_fields = {'slug': ('title', )}
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'shorttitle', 'slug', 'creator', 'created', 'thumbnail', 'module')
+    list_editable = ('title', 'shorttitle', 'slug', 'creator', 'created', 'thumbnail', 'module')
+    list_display_links = ('pk', )
+    list_filter = ( 'creator', 'module' )
+    search_fields = ('title', 'shorttitle')
 
     prepopulated_fields = {'slug': ('title', )}
