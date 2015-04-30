a8f3a310523f3c5ebb74a502a0e13d3a21d55d7b
Olivier Aubert <contact@olivieraubert.net>
2015-04-30 10:48:47 +0200

    Specify MEDIA_ROOT/MEDIA_URL

--- project/settings.py
+++ project/settings.py
@@ -15,6 +15,15 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(APPROOT, "media/")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
 
 # Application definition
 
