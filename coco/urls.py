7d7acb08e351ca3b299afd51d287058321141db8
Olivier Aubert <contact@olivieraubert.net>
2015-04-30 10:48:19 +0200

    Add more element links

--- coco/urls.py
+++ coco/urls.py
@@ -1,12 +1,25 @@
 from django.conf.urls import patterns, url, include
+from django.conf import settings
 from django.views.generic import TemplateView, ListView, DetailView
 import coco.views as views
-from .models import Course
+from .models import Course, Module, Activity, Video, Newsitem
 
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='root'),
+
+                       url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+
                        url(r'^course/$', ListView.as_view(model=Course), name='course-list'),
                        url(r'^course/(?P<pk>[\w\d_]+)/$', DetailView.as_view(model=Course, context_object_name='course'), name='course-detail'),
+                       url(r'^module/$', ListView.as_view(model=Module), name='module-list'),
+                       url(r'^module/(?P<pk>[\w\d_]+)/$', DetailView.as_view(model=Module, context_object_name='module'), name='module-detail'),
+                       url(r'^activity/$', ListView.as_view(model=Activity), name='activity-list'),
+                       url(r'^activity/(?P<pk>[\w\d_]+)/$', DetailView.as_view(model=Activity, context_object_name='activity'), name='activity-detail'),
+                       url(r'^video/$', ListView.as_view(model=Video), name='video-list'),
+                       url(r'^video/(?P<pk>[\w\d_]+)/$', DetailView.as_view(model=Video, context_object_name='video'), name='video-detail'),
+                       url(r'^news/$', ListView.as_view(model=Newsitem), name='newsitem-list'),
+                       url(r'^news/(?P<pk>[\w\d_]+)/$', DetailView.as_view(model=Newsitem, context_object_name='item'), name='newsitem-detail'),
+
                        # REST API
                        url(r'^api/', include(patterns('',
                                                       url(r'^course/$',
