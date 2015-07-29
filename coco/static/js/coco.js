d69585d3545fa4be9a0847f7f92690ae2851f5b5
Olivier Aubert <contact@olivieraubert.net>
2015-07-29 11:36:35 +0200

    Use Metadataplayer components

--- coco/static/js/coco.js
+++ coco/static/js/coco.js
@@ -1,7 +1,4 @@
 $(document).ready( function () {
-    var player = $("#videoplayer")[0];
-    document.player = player;
-
     function pad(n, x, b) {
         b = b || 10;
         var s = (x).toString(b);
@@ -34,39 +31,6 @@ $(document).ready( function () {
         return _res;
     };
 
-    $(player).on("pause", function () {
-        $(".playbutton").css({ "-webkit-transform": "scale(1)" });
-    }).on("play", function () {
-        $(".playbutton").css({"-webkit-transform": "scale(-1)" });
-    }).on("durationchange", function () {
-        $(".totaltime").text(format_timestamp(player.duration));
-    }).on("timeupdate", function () {
-        $(".currenttime").text(format_timestamp(player.currentTime));
-    });
-
-    $(".Ldt-AnnotationsList-li").on("click", function (e) {
-        e.preventDefault();
-        player.currentTime = this.dataset.begin / 1000;
-    });
-
-    $(".playbutton").on("click", function () {
-        console.log(this);
-        if (player.paused) {
-            player.play();
-        } else {
-            player.pause();
-        }
-    });
-    $(".fullscreenbutton").on("click", function () {
-        if (player.requestFullscreen) {
-            player.requestFullscreen();
-        } else if (player.mozRequestFullScreen) {
-            player.mozRequestFullScreen();
-        } else if (player.webkitRequestFullscreen) {
-            player.webkitRequestFullscreen();
-        }
-    });
-
     $(".tabnames li").on("click", function () {
         var tabname = Array.prototype.slice.call(this.classList).filter( function (s) { return s.indexOf("tab-") == 0; });
         if (tabname.length) {
@@ -75,14 +39,161 @@ $(document).ready( function () {
         }
     });
 
-    $(".tabcontrol_checkbox").on("click", function () {
-        var classname = Array.prototype.slice.call(this.classList).filter( function (s) { return s != "tabcontrol_checkbox"; });
-        if (classname.length) {
-            if ($(this).is(':checked')) {
-                $(".annotationlist ." + classname[0]).show(300);
-            } else {
-                $(".annotationlist ." + classname[0]).hide(300);
-            }
-        }
+    var username = localStorage.getItem('mla-username') || "Anonyme";
+    $("#username").val(username)
+        .on("blur", function () {
+            set_username($("#username").val());
+        });
+    function generateUuid () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
+    var user_uuid = localStorage.getItem('mla-uuid') || generateUuid();
+    localStorage.setItem('mla-uuid', user_uuid);
+
+    /* Customized metadataplayer configuration */
+    IriSP.libFiles.defaultDir = "../../static/libs/";
+    IriSP.widgetsDir = "../../static/metadataplayer";
+    IriSP.language = "fr";
+
+    var _config = {
+        width : '100%',
+        container : 'MiscControlsContainer',
+        default_options: {
+            metadata: metadata
+        },
+        css : IriSP.widgetsDir + '/LdtPlayer-core.css',
+        widgets: [
+            {
+                type: "HtmlPlayer",
+                video: metadata.video_url,
+                container: "VideoContainer",
+                width: '100%',
+                url_transform: function(n) {
+                    var elements = /(.+)\.(\w\w\w)$/.exec(n);
+                    var videoname = null;
+                    var v = document.createElement("video");
+                    if (v && v.canPlayType && v.canPlayType("video/mp4")) {
+                        videoname = elements[1] + ".mp4";
+                    } else {
+                        videoname = elements[1] + ".ogv";
+                    }
+                    return videoname;
+                }
+            },
+            { type: "Slider",
+              container: "ControlledVideoPlayer"
+            },
+            { type: "Controller",
+              container: "VideoControlContainer",
+              disable_annotate_btn: true,
+              always_show_search: true,
+              fullscreen_widget: '#VideoContainer'
+            },
+            {
+                type: "SlidePreview",
+                container: "SlidePreviewContainer",
+                annotation_type: "Slides"
+            },
+            {
+                type: "EnrichedPlan",
+                container: "AnnotationsContainer",
+                annotation_type: "Slides"
+            },
+            { type: "Trace",
+              url: "http://comin-ocw.org/trace/",
+              requestmode: "GET",
+              default_subject: "comin"
+            },
+            { type: "Mediafragment"},
+            { type: "Shortcuts"}
+        ]
+    }; //
+    _myPlayer = new IriSP.Metadataplayer(_config);
+    _myPlayer.on("trace-ready", function () {
+        var tracer = tracemanager.get_trace("test");
+        tracer.trace("PlayerStart", { url: document.URL });
+        IriSP.jQuery(".TraceMe").on("mousedown mouseenter mouseleave", function(_e) {
+            tracer.trace('Mdp_' + _e.type,
+                         {
+                             "widget": "coco",
+                             "target": this.id
+                         });
+        });
+        document.addEventListener("visibilitychange", function() {
+            tracer.trace("VisibilityChange", {
+                "state": document.visibilityState,
+                "url": document.URL
+            });
+        });
+
     });
+    document._myPlayer = _myPlayer;
+
+    function find_widgets_by_type(typ) {
+        if (_myPlayer.widgets) {
+            return _myPlayer.widgets.filter(function (w) {
+                return w.type == typ; });
+        } else {
+            return [];
+        }
+    }
+
+    function set_username(u) {
+        name = u;
+        localStorage.setItem('mla-username', u);
+        _myPlayer.config.username = u;
+        // Find CreateAnnotation widget and update creator_name
+        find_widgets_by_type("CreateAnnotation").forEach( function (w) { w.creator_name = u; });
+        find_widgets_by_type("Quiz").forEach( function (w) { w.user = u; w.userid=user_uuid; w.creator_name = u;});
+    };
+
+    var get_tab_index = function (id) {
+        var l = $("#tab > ul > li a").map(function (i, tab) { if (id == tab.getAttribute('href')) return i;});
+        // Return 0 index if the id is not found
+        return l[0] || 0;
+    };
+
+    /* Piwik */
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//comin-ocw.org/analytics/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', 1]);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+    /* Piwik */
+
+    var splitter, splitter2;
+    // Splitter between the Player/metadataplayer column and the tabs column
+    splitter =  $("#main").touchSplit({ barPosition: .66,
+                                        thickness: "22px",
+                                        secondMin: 400 })
+        .on("dragstart", function () {
+            $(this).find(".splitter-bar").addClass("active");
+        })
+        .on("dragstop", function () {
+            $(this).find(".splitter-bar").removeClass("active");
+            // on_resize();
+        });
+
+    /*
+    // Splitter between the video player and the rest of the metadataplayer
+    splitter2 = $("#PlayerContainer").touchSplit({orientation:"vertical", topMin: 220});
+    _myPlayer.on("widgets-loaded", function () {
+        set_username(name);
+        window.setTimeout(function () {
+            on_resize();
+        }, 500);
+    });
+
+    // Handle browser window resize
+    $(window).on("resize", function () { on_resize(); });
+
+     */
 });
