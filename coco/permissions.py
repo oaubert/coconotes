189a407327229e1f05b1f5e8a07992dfe060f943
Olivier Aubert <contact@olivieraubert.net>
2015-10-26 17:13:42 +0100

    Fix comment

--- coco/permissions.py
+++ coco/permissions.py
@@ -10,5 +10,5 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
+        # Write permissions are only allowed to the owner of the object
         return obj.creator == request.user
