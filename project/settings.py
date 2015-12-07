99893d6d46ca3f2157ddbbb0b0371385ef766ae2
Olivier Aubert <contact@olivieraubert.net>
2015-12-07 16:38:04 +0100

    Revert "Deactive messages middleware (unused)".
    It is used by the admin framework
    
    This reverts commit 6b1e2a4f90d58a540621a3701a99a70df93d67aa.

--- project/settings.py
+++ project/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-#    'django.contrib.messages',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
     'sorl.thumbnail',
@@ -81,7 +81,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
