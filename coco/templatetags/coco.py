aa0700ec87c3f353de08b9ac2824617355b9d2c3
Olivier Aubert <contact@olivieraubert.net>
2015-12-14 16:47:54 +0100

    Allow to specify formats for format_timecode templatetag

--- coco/templatetags/coco.py
+++ coco/templatetags/coco.py
@@ -7,9 +7,10 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+
 @register.filter
 @stringfilter
-def format_timecode(tc):
+def format_timecode(tc, format="short"):
     """Format a timecode/duration (in seconds)
     """
     if tc is None:
@@ -20,4 +21,11 @@ def format_timecode(tc):
     s = long(tc)
     ms = long(1000 * (tc - s))
     # Format: HH:MM:SS.mmm
-    return "%s.%03d" % (time.strftime("%H:%M:%S", time.gmtime(s)), ms)
+    tup = time.gmtime(s)
+    if format == 'short':
+        if tup.tm_hour > 0:
+            return time.strftime("%H:%M:%S", tup)
+        else:
+            return time.strftime("%M:%S", tup)
+    else:
+        return "%s.%03d" % (time.strftime("%H:%M:%S", tup), ms)
