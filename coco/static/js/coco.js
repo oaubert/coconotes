db52c077099ef1101640aa05b2e3b4e3d766bc2b
Olivier Aubert <contact@olivieraubert.net>
2016-01-11 17:12:48 +0100

    Display indicator for tab label overflow

--- coco/static/js/coco.js
+++ coco/static/js/coco.js
@@ -40,6 +40,16 @@ $(document).ready(function () {
         }
     });
 
+    var tablabels = $(".tabnames")[0];
+    function check_tablabels_overflow() {
+        if (tablabels.offsetHeight + 10 < tablabels.scrollHeight || tablabels.offsetWidth + 10 < tablabels.scrollWidth) {
+            // There is an overflow
+            $(tablabels).addClass("overflowing");
+        } else {
+            $(tablabels).removeClass("overflowing");
+        }
+    }
+    $(window).on("resize", check_tablabels_overflow);
     function generateUuid() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
@@ -352,8 +362,10 @@ $(document).ready(function () {
         })
         .on("dragstop", function () {
             $(this).find(".splitter-bar").removeClass("active");
-            // on_resize();
+            check_tablabels_overflow();
         });
+
+    check_tablabels_overflow();
 
     $(".videodetails").on("click touchstart", function (e) {
         e.stopPropagation();
