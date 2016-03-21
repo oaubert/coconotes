309d525dba4d71c593d228821652c17c7b8adde7
Olivier Aubert <contact@olivieraubert.net>
2016-03-21 15:46:47 +0100

    Add local_settings-example file

---
+++ project/local_settings-example.py
@@ -0,0 +1,11 @@
+# Local settings. Any key that is not defined here will get a default value (see settings.py)
+options = {
+    'secret_key': '--generate a secret key--lz9u^$sca56-uyr=9*-!pdmyvmr23*=2@c#1#zsg6@c!0h^k*7',
+    'development': True,
+    'db_engine': 'django.db.backends.sqlite3',
+    'db_name': 'coconotes',
+    'db_user': '',
+    'db_password': '',
+    'db_host': '',
+    'db_port': '',
+    }
